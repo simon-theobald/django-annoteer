d2dd0d5d127ddd4f603fa208e2745bc093522193
Ivan <Ivan.Dranyshov@stud.uni-regensburg.de>
2025-05-20 09:27:29 +0200

    U1 Model for a text

---
+++ dataitem/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Dataitem
+
+# Register your models here.
+admin.site.register(Dataitem)
+from django.contrib import admin
+
+# Register your models here.
